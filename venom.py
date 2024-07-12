30e98fddacf8e1de12bfa1fadeed9178b1d84863
tanishdroid <119420550+tanishdroid@users.noreply.github.com>
2024-07-12 17:47:56 +0530

    venom.py

--- venom.py
+++ venom.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = 'TOKEN'
+TOKEN = '6444547071:AAEQxbmnfSmya_ST4JJ5l31B535R0RpanYM'
 MONGO_URI = 'mongodb+srv://VENOMxCRAZY:CRAZYxVENOM@cluster0.ythilmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -1002224570220
-CHANNEL_ID = -1002224570220
-error_channel_id = -1002224570220
+FORWARD_CHANNEL_ID = -1002164618223
+CHANNEL_ID = -1002164618223
+error_channel_id = -1002164618223
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
